f355c1fc24201a22e0c0c7ec41fc6a05b6be0030
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-07 12:26:29 -0800

    Implement everything to make all tests pass for BeesBeesBees

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.color = 'yellow';
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = new Grub();
+HoneyMakerBee.prototype.makeHoney = function(honeyPot) {
+  return this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function(honeyPot) {
+  return this.honeyPot--;
+};
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,16 @@
 var RetiredForagerBee = function() {
-};
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 
+};
+RetiredForagerBee.prototype = new Grub();
+RetiredForagerBee.prototype.forage = function(treasure) {
+  return 'I am too old, let me play cards instead';
+};
+RetiredForagerBee.prototype.gamble = function(treasure) {
+   return this.treasureChest.push(treasure);
+};
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
