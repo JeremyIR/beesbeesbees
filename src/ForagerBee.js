982a85811b20d9a73cd3a545c0912a150c820dec
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-04-24 15:28:03 -0700

    clean up code

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,13 +1,13 @@
 var ForagerBee = function() {
-  HoneyMakerBee.call(this);
-  this.age = 10;
-  this.job = 'find pollen';
-  this.canFly = true;
-  this.treasureChest = [];
+    HoneyMakerBee.call(this);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
 };
 
 ForagerBee.prototype = Object.create(Grub.prototype);
 ForagerBee.prototype.forage = function(treasure) {
-  this.treasureChest.push(treasure);
+    this.treasureChest.push(treasure);
 };
 ForagerBee.prototype.constructor = ForagerBee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,17 +1,17 @@
 var HoneyMakerBee = function() {
-  Bee.call(this);
-  this.age = 10;
-  this.color = 'yellow';
-  this.job = 'make honey';
-  this.honeyPot = 0;
+    Bee.call(this);
+    this.age = 10;
+    this.color = 'yellow';
+    this.job = 'make honey';
+    this.honeyPot = 0;
 };
 
 HoneyMakerBee.prototype = Object.create(Grub.prototype);
 HoneyMakerBee.prototype.makeHoney = function(honeyPot) {
-  return this.honeyPot++;
+    return this.honeyPot++;
 };
 
 HoneyMakerBee.prototype.giveHoney = function(honeyPot) {
-  return this.honeyPot--;
+    return this.honeyPot--;
 };
 HoneyMakerBee.prototype.constructor = HoneyMakerBee;
